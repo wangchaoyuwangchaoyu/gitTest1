78c1aad452a817d52603fb1a057b2b90163d18db
wangchaoyu <wangchaoyu@360.net>
2018-10-18 10:49:04 +0800

    添加项目文件。

---
+++ gitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
